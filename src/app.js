69cb0b7fba60a251763ec3e7cb8b717f0a714db7
Pradeep Etika <etikapradeep@gmail.com>
2026-04-17 10:20:14 +0530

    exploring get, post, patch, delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Pradeep");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Pradeep", lastName: "Etika" });
 });
 
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  //saving data to DB
+  res.send("Data successfully saved on database!");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data updated successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully!");
+});
+
+//this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
-  res.send("test from the server...");
+  res.send("Hello Pradeep");
 });
 
 app.listen(3000, () => {
