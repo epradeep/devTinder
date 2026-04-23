d8078df5aabe5ccaa8bc9ec3b096de4e085753aa
Pradeep Etika <etikapradeep@gmail.com>
2026-04-23 10:12:43 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Pradeep", lastName: "Etika" });
-});
+//app.use("/route", rh1, [rh2,rh3], rh4);
+
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Route Handler 1");
+      // res.send("Route Handler 1");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Route Handler 2");
+      // res.send("Route Handler 2");
+      next();
+    },
+  ],
+  (req, res) => {
+    console.log("Route Handler 3");
+    res.send("Route Handler 3");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server connected successfully on port 3000...");
