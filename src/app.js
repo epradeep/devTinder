72e02eb922f9fe2d9b75a8ea3765c2409c336908
Pradeep Etika <etikapradeep@gmail.com>
2026-04-23 05:38:26 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,27 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Pradeep", lastName: "Etika" });
-});
-
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  //saving data to DB
-  res.send("Data successfully saved on database!");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Data updated successfully!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data deleted successfully!");
-});
-
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello Pradeep");
 });
 
 app.listen(3000, () => {
