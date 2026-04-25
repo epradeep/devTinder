affd9fdc6e3a11e3a8f37424444623bccb0196cf
Pradeep Etika <etikapradeep@gmail.com>
2026-04-25 16:59:05 +0530

    update &delete APIs

--- src/app.js
+++ src/app.js
@@ -50,6 +50,30 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+//Delete user from database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    console.log(user);
+    res.send("User updated successfully!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
